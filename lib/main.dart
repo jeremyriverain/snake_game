e9017c7307f7c102d4da13371c5c91460ebd3a4f
Jérémy Riverain <jeremy@geekco.fr>
2023-06-28 16:33:48 +0200

    🔒️ lock orientation portait

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:snake_game/snake_game.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(
     SafeArea(
       child: GameWidget(
